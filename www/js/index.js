3a304ca481f48a4fb1d9a7a7fe2d7f0b730410d3
Chico Charlesworth <chico.charlesworth@googlemail.com>
2013-08-16 18:52:41 +0100

    trying to fix

--- www/js/index.js
+++ www/js/index.js
@@ -30,12 +30,13 @@ var app = {
         console.log('bindEvents');
         document.addEventListener('deviceready', function() {
             console.log('deviceready');
-            
+
             var xhr = new XMLHttpRequest();
             xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
             // Response handlers.
             xhr.onload = function () {
-                var repos = JSON.parse(xhr.response), i, reposHTML = "";
+                var responseStr = xhr.respose || xhr.responseText,
+                    repos = JSON.parse(responseStr), i, reposHTML = "";
                 for (i = 0; i < repos.repositories.length; i++) {
                 reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
                 }
