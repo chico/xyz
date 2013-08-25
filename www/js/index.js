c7d1ba568815275f3ad15449888897d7f1632f4e
Chico Charlesworth <chico.charlesworth@googlemail.com>
2013-08-25 08:11:03 +0100

    fixing js when transitioning to setup page

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,51 @@
-$('#reposHome').bind('pageinit', function(event) {
-    console.log('pageinit');
-    loadRepos();
+
+$('#setupPage').live('pageshow', function(event) {    
+    alert("pageshow");
+    initDatePicker();
 });
 
-function loadRepos() {
-    $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-        var i, repo;
-        $.each(data.repositories, function (i, repo) {
-            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
-            + "<h4>" + repo.name + "</h4>"
-            + "<p>" + repo.username + "</p></a></li>");
-        });
-        $('#allRepos').listview('refresh');
+function initDatePicker() {
+    yepnope({
+        test: Modernizr.inputtypes.date && Modernizr.inputtypes.time,
+        nope: [
+            'js/jquery.mousewheel.js',
+            'js/jqm-datebox.core.js',
+            'js/jqm-datebox.mode.calbox.js',
+            'js/jqm-datebox.mode.datebox.js',
+            'js/jquery.mobile.datebox.i18n.en_US.utf8.js'],
+        complete: function(url, result, key) {
+            
+            if (!Modernizr.inputtypes.date) {
+                // polyfill the date inputs
+                $( document ).on( "pagecreate create", function( e ){
+                    $( "input[type='date']", e.target ).each(function() {
+                        if ( typeof($(this).data('datebox')) === "undefined" ) {
+                            $(this).datebox();
+                        }
+                    });
+                });
+                $("input[type='date']").each(function() {
+                    if (typeof($(this).data('datebox')) === "undefined") {
+                        $(this).datebox();
+                    }
+                });
+            }
+        
+            if (!Modernizr.inputtypes.time) {
+                // polyfill the time inputs
+                $( document ).on( "pagecreate create", function( e ){
+                    $( "input[type='time']", e.target ).each(function() {
+                        if ( typeof($(this).data('datebox')) === "undefined" ) {
+                            $(this).datebox();
+                        }
+                    });
+                });
+                $("input[type='time']").each(function() {
+                    if (typeof($(this).data('datebox')) === "undefined") {
+                        $(this).datebox();
+                    }
+                });
+            }
+        }
     });
 }
