e877c25c514710536994d29b30940a3c25492598
Chico Charlesworth <chico.charlesworth@googlemail.com>
2013-08-29 16:56:18 +0100

    hooking in camera

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,13 @@
 
+console.log("init");
+
+$('#photoPage').live('pageshow', function(event) {
+    console.log("trigger initCamera");
+    initCamera();
+});
+
 $('#setupPage').live('pageshow', function(event) {
+    console.log("trigger initDatePicker");
     initDatePicker();
 });
 
@@ -48,4 +56,34 @@ function initDatePicker() {
             }
         }
     });
+}
+
+function initCamera() {
+
+    console.log("initCamera");
+    alert("initCamera");
+
+    var onSuccess = function(uri) {
+        alert(uri);
+    };
+
+    var onFail = function() {
+        console.log('Failed to get an image');
+    };
+
+    $('.image-upload').bind('tap', function() {
+        console.log("upload");
+        alert("upload");
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: navigator.camera.DestinationType.FILE_URI,
+            sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+        });
+    });
+
+    $('.image-upload btn').bind('click', function() {
+        console.log("btn click");
+        alert("btn click");
+    });
+
 }
