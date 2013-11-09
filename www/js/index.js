d2e0699f7f58f4e269121c707b6e4854c7c6c21d
Chico Charlesworth <chico.charlesworth@googlemail.com>
2013-11-09 16:08:03 +0000

    setting local flag to false

--- www/js/index.js
+++ www/js/index.js
@@ -8,7 +8,7 @@ var photos = [
     {photo:undefined, caption:''}
 ];
 
-var local = true;
+var local = false;
 var testPhotos = [
     {photo:'http://farm4.staticflickr.com/3767/9056854173_28bbd5c2cb_n.jpg', caption:''},
     {photo:'http://farm3.staticflickr.com/2835/8926405863_8b4afb79b9_m.jpg', caption:''},
